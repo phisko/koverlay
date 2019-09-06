4d615dedee393be13d98113e6d3ad8561639311a
Nicolas Phister] <nicolas@phister.fr>
2019-09-06 10:45:02 +0200

    made it possible to toggle scripts like other tools

--- scripts/test.lua
+++
@@ -1,5 +0,0 @@
-if imgui.Begin("Lua") then
-	imgui.BeginChild("child", 0, 50 * IMGUI_SCALE, true)
-	imgui.EndChild()
-end
-imgui.End()
--- src/ImGuiLuaSystem.cpp
+++ src/ImGuiLuaSystem.cpp
@@ -9,6 +9,43 @@ static float getScale() {
 	return g_scale != nullptr ? *g_scale : 1.f;
 }
 
+struct ToolInfo {
+	std::string name;
+	bool enabled;
+};
+
+static ToolInfo & getToolInfo(const std::string & scriptName, bool & shouldRegisterTool) {
+	static std::unordered_map<std::string, ToolInfo *> scriptToolMap;
+
+	ToolInfo * toolInfo = nullptr;
+
+	if (scriptToolMap.find(scriptName) == scriptToolMap.end()) {
+		toolInfo = new ToolInfo{ "", true };
+		scriptToolMap[scriptName] = toolInfo;
+		shouldRegisterTool = true;
+	}
+	else
+		toolInfo = scriptToolMap[scriptName];
+
+	return *toolInfo;
+}
+
+static void runScript(const std::string & scriptName, kengine::EntityManager & em, sol::state & state) {
+	bool shouldRegisterTool = false;
+	auto & toolInfo = getToolInfo(scriptName, shouldRegisterTool);
+
+	if (toolInfo.enabled) {
+		state["TOOL_ENABLED"] = true;
+		state.script_file(scriptName);
+		toolInfo.enabled = state["TOOL_ENABLED"];
+
+		if (shouldRegisterTool) {
+			toolInfo.name = state["TOOL_NAME"];
+			em.send(kengine::packets::AddImGuiTool{ toolInfo.name.c_str(), toolInfo.enabled });
+		}
+	}
+}
+
 ImGuiLuaSystem::ImGuiLuaSystem(kengine::EntityManager & em) : System(em), _em(em) {
 	extern lua_State * lState;
 	extern void LoadImguiBindings();
@@ -16,7 +53,6 @@ ImGuiLuaSystem::ImGuiLuaSystem(kengine::EntityManager & em) : System(em), _em(em
 	auto & state = em.getSystem<kengine::LuaSystem>().getState();
 	lState = state;
 	LoadImguiBindings();
-
 
 	em += [&](kengine::Entity & e) {
 		e += kengine::ImGuiComponent([&] {
@@ -28,12 +64,8 @@ ImGuiLuaSystem::ImGuiLuaSystem(kengine::EntityManager & em) : System(em), _em(em
 				const auto view = std::string_view(f.name);
 				const auto dot = view.find_last_of('.');
 				if (!f.isDirectory && dot != std::string_view::npos && view.substr(dot) == ".lua")
-					state.script_file(f.fullPath.c_str());
+					runScript(f.fullPath.c_str(), em, state);
 			});
-
-			// bool * enabled;
-			// for (const auto & name : names)
-			// 	send(kengine::packets::AddImGuiTool{ name, *enabled });
 		});
 	};
 }
