932a70b2565c9c6ad2504dcc6cc959fded261ef0
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-03-18 16:45:54 +0100

    update example.lua

--- examples/example.lua
+++ examples/example.lua
@@ -1,9 +1,18 @@
 TOOL_NAME = "Lua"
 -- can use IMGUI_SCALE to properly scale child windows
 
+if EXAMPLE_BUTTON_CLICKED == nil then
+    EXAMPLE_BUTTON_CLICKED = false
+end
+
 shouldDraw, TOOL_ENABLED = imgui.Begin("Lua", TOOL_ENABLED)
 if shouldDraw then
-	imgui.BeginChild("child", 0, 50 * IMGUI_SCALE, true)
-	imgui.EndChild()
+    if imgui.SmallButton("Click me") then
+        EXAMPLE_BUTTON_CLICKED = true
+    end
+
+    if EXAMPLE_BUTTON_CLICKED then
+        imgui.TextUnformatted("Good job!")
+    end
 end
 imgui.End()
