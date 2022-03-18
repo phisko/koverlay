1e23464a01ad69edb05ca1303732c2360100e955
Nicolas Phister <nicolas.phister@dont-nod.com>
2022-03-18 16:44:58 +0100

    kengine update + anonymous namespaces

--- src/ImGuiLuaSystem.cpp
+++ src/ImGuiLuaSystem.cpp
@@ -13,86 +13,94 @@
 // putils
 #include "Directory.hpp"
 
-struct impl {
-	static inline sol::state * g_state = nullptr;
+namespace {
+    struct impl {
+        static inline sol::state *g_state = nullptr;
 
-	static void init(kengine::Entity & system) noexcept {
-		initBindings();
-		system += kengine::functions::Execute{ [&](float deltaTime) noexcept {
-			runScripts();
-		} };
-	}
+        static void init(kengine::Entity &system) noexcept {
+            initBindings();
+            system += kengine::functions::Execute{[&](float deltaTime) noexcept {
+                runScripts();
+            }};
+        }
 
-	static void initBindings() noexcept {
-		extern lua_State * lState;
-		extern void LoadImguiBindings();
+        static void initBindings() noexcept {
+            extern lua_State *lState;
+            extern void LoadImguiBindings();
 
-		for (const auto & [e, state] : kengine::entities.with<kengine::LuaStateComponent>()) {
-			lState = *state.state;
-			LoadImguiBindings();
+            for (const auto &[e, state]: kengine::entities.with<kengine::LuaStateComponent>()) {
+                lState = *state.state;
+                LoadImguiBindings();
+                g_state = state.state;
+                g_state->script(
+                        R"(
+print("ImGui Lua bindings:")
+for k, v in pairs(imgui) do
+    print("\t", k)
+end
+)");
+            }
+        }
 
-			g_state = state.state;
-		}
-	}
+        static void runScripts() noexcept {
+            (*g_state)["IMGUI_SCALE"] = getScale();
 
-	static void runScripts() noexcept {
-		(*g_state)["IMGUI_SCALE"] = getScale();
+            putils::Directory d("scripts");
 
-		putils::Directory d("scripts");
+            d.for_each([&](const putils::Directory::File &f) {
+                const auto view = std::string_view(f.name);
+                const auto dot = view.find_last_of('.');
+                if (!f.isDirectory && dot != std::string_view::npos && view.substr(dot) == ".lua")
+                    runScript(f.fullPath.c_str());
+            });
+        }
 
-		d.for_each([&](const putils::Directory::File & f) {
-			const auto view = std::string_view(f.name);
-			const auto dot = view.find_last_of('.');
-			if (!f.isDirectory && dot != std::string_view::npos && view.substr(dot) == ".lua")
-				runScript(f.fullPath.c_str());
-		});
-	}
+        static float getScale() noexcept {
+            float scale = 1.f;
+            for (const auto &[e, comp]: kengine::entities.with<kengine::ImGuiScaleComponent>())
+                scale *= comp.scale;
+            return scale;
+        }
 
-	static float getScale() noexcept {
-		float scale = 1.f;
-		for (const auto & [e, comp] : kengine::entities.with<kengine::ImGuiScaleComponent>())
-			scale *= comp.scale;
-		return scale;
-	}
+        static void runScript(const char *script) noexcept {
+            auto e = getEntityForScript(script);
+            auto &tool = e.get<kengine::ImGuiToolComponent>();
 
-	static void runScript(const char * script) noexcept {
-		auto e = getEntityForScript(script);
-		auto & tool = e.get<kengine::ImGuiToolComponent>();
+            if (!tool.enabled)
+                return;
 
-		if (!tool.enabled)
-			return;
+            (*g_state)["TOOL_ENABLED"] = tool.enabled;
+            try {
+                g_state->script_file(script);
+            }
+            catch (const std::exception &e) {
+                std::cerr << e.what() << std::endl;
+            }
+            tool.enabled = (*g_state)["TOOL_ENABLED"];
+        }
 
-		(*g_state)["TOOL_ENABLED"] = tool.enabled;
-		try {
-			g_state->script_file(script);
-		}
-		catch (const std::exception & e) {
-			std::cerr << e.what() << std::endl;
-		}
-		tool.enabled = (*g_state)["TOOL_ENABLED"];
-	}
+        static kengine::Entity getEntityForScript(const char *script) noexcept {
+            static std::unordered_map<std::string, kengine::EntityID> ids;
 
-	static kengine::Entity getEntityForScript(const char * script) noexcept {
-		static std::unordered_map<std::string, kengine::EntityID> ids;
+            const auto it = ids.find(script);
+            if (it != ids.end())
+                return kengine::entities[it->second];
 
-		const auto it = ids.find(script);
-		if (it != ids.end())
-			return kengine::entities[it->second];
-
-		return kengine::entities.create([&](kengine::Entity & e) {
-			ids[script] = e.id;
-			try {
-				g_state->script_file(script);
-			}
-			catch (const std::exception & e) {
-				std::cerr << e.what() << std::endl;
-			}
-			e += kengine::ImGuiToolComponent{ (*g_state)["TOOL_ENABLED"] };
-			const std::string name = (*g_state)["TOOL_NAME"];
-			e += kengine::NameComponent{ name };
-		});
-	}
-};
+            return kengine::entities.create([&](kengine::Entity &e) {
+                ids[script] = e.id;
+                try {
+                    g_state->script_file(script);
+                }
+                catch (const std::exception &e) {
+                    std::cerr << e.what() << std::endl;
+                }
+                e += kengine::ImGuiToolComponent{(*g_state)["TOOL_ENABLED"]};
+                const std::string name = (*g_state)["TOOL_NAME"];
+                e += kengine::NameComponent{name};
+            });
+        }
+    };
+}
 
 kengine::EntityCreator * ImGuiLuaSystem() noexcept {
 	return impl::init;
--- src/ImGuiPluginSystem.cpp
+++ src/ImGuiPluginSystem.cpp
@@ -14,48 +14,50 @@
 struct ImGuiContext;
 extern ImGuiContext * GImGui;
 
-struct impl {
-	static inline putils::PluginManager pm;
+namespace {
+    struct impl {
+        static inline putils::PluginManager pm;
 
-	static void init(kengine::Entity & system) noexcept {
-		system += kengine::functions::Execute{ execute };
-	}
+        static void init(kengine::Entity &system) noexcept {
+            system += kengine::functions::Execute{execute};
+        }
 
-	struct PluginEnabledPointerComponent {
-		bool * enabled;
-	};
+        struct PluginEnabledPointerComponent {
+            bool *enabled;
+        };
 
-	static void execute(float deltaTime) noexcept {
-		bool * enabled;
-		const auto names = pm.rescanDirectoryWithReturn<1, const char *>("plugins", "getNameAndEnabled", &enabled);
-		for (const auto & name : names) {
-			kengine::entities += [&](kengine::Entity & e) {
-				e += kengine::NameComponent{ name };
-				e += PluginEnabledPointerComponent{ enabled };
-				e += kengine::ImGuiToolComponent{ *enabled };
-			};
-		}
+        static void execute(float deltaTime) noexcept {
+            bool *enabled;
+            const auto names = pm.rescanDirectoryWithReturn<1, const char *>("plugins", "getNameAndEnabled", &enabled);
+            for (const auto &name: names) {
+                kengine::entities += [&](kengine::Entity &e) {
+                    e += kengine::NameComponent{name};
+                    e += PluginEnabledPointerComponent{enabled};
+                    e += kengine::ImGuiToolComponent{*enabled};
+                };
+            }
 
-		drawImGui();
-	}
+            drawImGui();
+        }
 
-	static void drawImGui() noexcept {
-		for (const auto & [e, enabled, tool] : kengine::entities.with<PluginEnabledPointerComponent, kengine::ImGuiToolComponent>())
-			*enabled.enabled = tool.enabled;
+        static void drawImGui() noexcept {
+            for (const auto &[e, enabled, tool]: kengine::entities.with<PluginEnabledPointerComponent, kengine::ImGuiToolComponent>())
+                *enabled.enabled = tool.enabled;
 
-		pm.execute("drawImGui", *GImGui, getScale());
+            pm.execute("drawImGui", *GImGui, getScale());
 
-		for (const auto & [e, enabled, tool] : kengine::entities.with<PluginEnabledPointerComponent, kengine::ImGuiToolComponent>())
-			tool.enabled = *enabled.enabled;
-	}
+            for (const auto &[e, enabled, tool]: kengine::entities.with<PluginEnabledPointerComponent, kengine::ImGuiToolComponent>())
+                tool.enabled = *enabled.enabled;
+        }
 
-	static float getScale() noexcept {
-		float scale = 1.f;
-		for (const auto & [e, comp] : kengine::entities.with<kengine::ImGuiScaleComponent>())
-			scale *= comp.scale;
-		return scale;
-	}
-};
+        static float getScale() noexcept {
+            float scale = 1.f;
+            for (const auto &[e, comp]: kengine::entities.with<kengine::ImGuiScaleComponent>())
+                scale *= comp.scale;
+            return scale;
+        }
+    };
+}
 
 kengine::EntityCreator * ImGuiPluginSystem() noexcept {
 	return impl::init;
